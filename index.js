89b5642fcc3bc253a97e6d3630d048634dd73299
kemalhiero <kemalhiero@gmail.com>
2024-03-22 13:48:12 +0700

    bismillah

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+//halaman root
+app.get('/', (req, res) => {
+    res.send('halo halo')
+});
+
+// Server
+app.listen(port, () => {
+    console.log(`Server berjalan pada http://localhost:${port}`);
+});
